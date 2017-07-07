93dc88d8e943fbd703c42504eefd092db9eb888e
0305zhanna <0305zhanna@mail.ru>
2017-07-07 15:25:48 +0300

    Create classes fork and philosopher

---
+++ src/lesson170705/Homework/Fork.java
@@ -0,0 +1,8 @@
+package lesson170705.Homework;
+
+/**
+ * Created by User on 07.07.2017.
+ */
+public class Fork {
+
+}
---
+++ src/lesson170705/Homework/Philosopher.java
@@ -0,0 +1,7 @@
+package lesson170705.Homework;
+
+/**
+ * Created by User on 07.07.2017.
+ */
+public class Philosopher {
+}
